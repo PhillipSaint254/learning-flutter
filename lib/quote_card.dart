0792c90aeb6621b999576f16e464ae53fa56fabc
phillip saint <phillipsantei254@gmail.com>
2024-02-26 16:31:38 +0300

    added code to pass functions as parameters to other widgets

--- lib/quote_card.dart
+++ lib/quote_card.dart
@@ -3,7 +3,8 @@ import 'quote.dart';
 
 class QuoteCard extends StatelessWidget {
   final Quote quote;
-  QuoteCard({ required this.quote });
+  final Function delete;
+  QuoteCard({ required this.quote, required this.delete });
 
   @override
   Widget build(BuildContext context) {
@@ -28,6 +29,12 @@ class QuoteCard extends StatelessWidget {
                       fontSize: 14.0,
                       color: Colors.grey[800]
                   )
+              ),
+              SizedBox(height: 8.0),
+              TextButton.icon(
+                onPressed: () => delete(),
+                label: Text("delete quote"),
+                icon: Icon(Icons.delete),
               )
             ],
           ),
