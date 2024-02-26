81e58ce0c809a88856b9b0e4184addfa67d4f875
phillip saint <phillipsantei254@gmail.com>
2024-02-26 15:15:13 +0300

    Addition of a custom class to hold quote data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -15,10 +16,10 @@ class QuoteList extends StatefulWidget {
 
 class _QuoteListState extends State<QuoteList> {
 
-  List<String> quotes = [
-    'Be yourself; everyone else is already taken',
-    'I have nothing to declare except my genius',
-    'The truth is rarely pure and never simple',
+  List<Quote> quotes = [
+    Quote(text: 'Be yourself; everyone else is already taken', author: 'Osca Wilde'),
+    Quote(text: 'I have nothing to declare except my genius', author: 'Osca Wilde'),
+    Quote(text: 'The truth is rarely pure and never simple', author: 'Osca Wilde'),
   ];
 
   @override
@@ -31,7 +32,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => Text(quote)).toList(),
+        children: quotes.map((quote) => Text('${quote.text} <${quote.author}>')).toList(),
       ),
     );
   }
