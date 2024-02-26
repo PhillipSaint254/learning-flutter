79070c8a8b92675ddd60921e5df0fc74d8034967
phillip saint <phillipsantei254@gmail.com>
2024-02-26 16:03:59 +0300

    added an external stateless widget for the card to make the code more reusable

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'quote.dart';
+import 'quote_card.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -22,35 +23,6 @@ class _QuoteListState extends State<QuoteList> {
     Quote(text: 'The truth is rarely pure and never simple', author: 'Osca Wilde'),
   ];
 
-  Widget quoteTemplate(quote) {
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0,16.0,16.0,0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-                quote.text,
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.grey[500]
-              )
-            ),
-            SizedBox(height: 6.0),
-            Text(
-              quote.author,
-              style: TextStyle(
-                fontSize: 14.0,
-                color: Colors.grey[800]
-              )
-            )
-          ],
-        ),
-      )
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,7 +33,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+        children: quotes.map((quote) => QuoteCard(quote: quote,)).toList(),
       ),
     );
   }
