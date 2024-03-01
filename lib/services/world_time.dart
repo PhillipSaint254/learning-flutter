b168624e29ea6d5cbf857403f031dfcc50c79794
phillip saint <phillipsantei254@gmail.com>
2024-03-01 19:24:27 +0300

    Formatting datetime to a human readable format and styling home page

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime {
   String location; //name for the UI
@@ -26,7 +27,7 @@ class WorldTime {
       now = now.add(Duration(hours: offsetHours));
 
       //set the time property
-      time = now.toString();
+      time = DateFormat.jm().format(now);
     }
     catch (e) {
       print("caught error: $e");
