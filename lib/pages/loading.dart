f3f22364313416ccf417601eacc8a9ddea45bd6b
phillip saint <phillipsantei254@gmail.com>
2024-03-01 17:05:55 +0300

    connected to the world time api online

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -11,18 +11,30 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
-    Response response = await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+  void getTime() async {
+    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/Africa/Nairobi'));
     Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
+
+// get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3); // Extract offset without the sign
+    print(offset);
+
+    DateTime now = DateTime.parse(datetime);
+    print('Original time: $now');
+
+// Parse offset and add it to the DateTime object
+    int offsetHours = int.parse(offset);
+    now = now.add(Duration(hours: offsetHours));
+    print('Adjusted time: $now');
   }
+
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
