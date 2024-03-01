8b70a1c1dde1f271d6bf43c9310c0f435a077a7e
phillip saint <phillipsantei254@gmail.com>
2024-03-01 16:19:39 +0300

    Asynchronous code to practice waiting for data from third party APIs

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -5,9 +5,33 @@ class ChooseLocation extends StatefulWidget {
 
   @override
   State<ChooseLocation> createState() => _ChooseLocationState();
+
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  void getData() async {
+    // simulating a network request for a username
+    String username = await Future.delayed(Duration(seconds: 3), () {
+      return "Yoshi";
+    });
+
+    // simulate network request to get bio for username
+    String bio = await Future.delayed(Duration(seconds: 2), () {
+      return "vegan, musician and shell collector";
+    });
+
+    print("$username - '$bio'");
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+    print("smh");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
